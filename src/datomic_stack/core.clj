97170b98d2328e7217dae60ac1eefeba5b00bec8
Andrej Lamov <andrej.lamov@gmail.com>
2018-01-22 00:57:58 +0100

    Use async/put! when just putting to channel

--- src/datomic_stack/core.clj
+++ src/datomic_stack/core.clj
@@ -42,11 +42,11 @@
         "logout" (logout db-conn out->user-name out)
         "register" (register db-conn kv-args out->user-name out)
         "login" (login db-conn kv-args out->user-name out)
-        (go (>! out {:user/logged-in? true :cmd.error/name (str cmd " command not found") })))
+        (async/put! out {:user/logged-in? true :cmd.error/name (str cmd " command not found") }))
       (case cmd
         "register" (register db-conn kv-args out->user-name out)
         "login" (login db-conn kv-args out->user-name out)
-        (go (>! out {:user/logged-in? false :cmd.error/name (str cmd " command not found") }))
+        (async/put! out {:user/logged-in? false :cmd.error/name (str cmd " command not found") })
         ))))
 
 (defn share-group [db-conn {:keys [:user/name :group/name]} user-name out]
@@ -67,15 +67,14 @@
                              user-name
                              (conj (or can-read #{}) user-name)
                              (conj (or can-upsert #{}) user-name)))
-      (go (>! out {:tx/completed true})))
+  (async/put! out {:tx/completed true}))
 
 (defn logged-in? [out->user-name out]
   (contains? @out->user-name out))
 
 (defn logout [db-conn out->user-name out]
-  (go
-    (let [res (swap! out->user-name dissoc out)]
-      (>! out {:user/logged-in? (contains? res out)}))))
+  (let [res (swap! out->user-name dissoc out)]
+    (async/put! out {:user/logged-in? (contains? res out)})))
 
 (defn login [db-conn
              {:keys [:user/name :user/password] :as expected}
@@ -89,30 +88,29 @@
     (when logged-in?
       (swap! out->user-name assoc out name)
       )
-    (go (>! out {:user/logged-in? logged-in?
-                 :user/name name}))))
+    (async/put! out {:user/logged-in? logged-in?
+             :user/name name})))
 
 (defn register [db-conn
                 {:keys [:user/name] :as user}
                 out->user-name
                 out]
-  (go
-    (let [{:keys [:db/id] :as existing} (d/pull
-                                         (d/db db-conn)
-                                         [:db/id]
-                                         [:user/name name])]
-      (if id
-        (>! out {:user/logged-in? false
-                 :error.user/name "name already taken"})
-        (do
-          (rd/restricted-transact db-conn
-                                  user
-                                  name
-                                  (rd/tx-data name
-                                              #{name}
-                                              #{name}))
-          (login db-conn user out->user-name out)))
-      )))
+  (let [{:keys [:db/id] :as existing} (d/pull
+                                       (d/db db-conn)
+                                       [:db/id]
+                                       [:user/name name])]
+    (if id
+      (async/put! out {:user/logged-in? false
+                       :error.user/name "name already taken"})
+      (do
+        (rd/restricted-transact db-conn
+                                user
+                                name
+                                (rd/tx-data name
+                                            #{name}
+                                            #{name}))
+        (login db-conn user out->user-name out)))
+    ))
 
 (defn fresh-db [schema]
   (let [db-name (gensym)
